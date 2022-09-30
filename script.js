d4267905a20004611a5f49499265207f72dda29d
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-30 13:11:19 -0700

    added flexbox properties so square size can change

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function createSquare() {
     const square = document.createElement("div");
-    square.setAttribute("style","background-color: white; border: black solid 1px; width: 30px; height: 30px;");
+    square.setAttribute("style","background-color: white; border: black solid 1px; flex: 1 1 auto;");
     square.addEventListener("mouseover", event => {
-        event.target.setAttribute("style","background-color: gray; border: black solid 1px; width: 30px; height: 30px;")
+        event.target.setAttribute("style","background-color: gray; border: black solid 1px; flex: 1 1 auto;")
     });
     return square;
 }
 
 function createRow() {
     const row = document.createElement("div");
-    row.setAttribute("style","display: flex; justify-content: center;");
+    row.setAttribute("style","display: flex; justify-content: center; width: 560px; flex: 1 1 auto;");
 
     for (let i = 0; i < 16; i++) {
         const square = createSquare();
@@ -22,7 +22,7 @@ function createRow() {
 function createGrid() {
     const attachGrid = document.getElementById("attachGrid");
     const grid = document.createElement("div");
-    grid.setAttribute("style", "display: flex; flex-direction: column;")
+    grid.setAttribute("style", "display: flex; flex-direction: column; align-items: center; height: 560px;")
     
     for (let i = 0; i < 16; i++) {
         const row = createRow();
